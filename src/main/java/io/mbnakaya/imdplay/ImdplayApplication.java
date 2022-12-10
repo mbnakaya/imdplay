80cd1bfa7ebab06fd0bc5ae24a2049a189e05195
Matheus Nakaya <mbnakaya@localhost.localdomain>
2022-12-10 20:40:28 -0300

    Start project

---
+++ src/main/java/io/mbnakaya/imdplay/ImdplayApplication.java
@@ -0,0 +1,13 @@
+package io.mbnakaya.imdplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/mbnakaya/imdplay/ImdplayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mbnakaya.imdplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
